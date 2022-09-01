82b1faa09f5cf6b44da8ebe100a9530e7ec084bf
spangen87 <rikard.spangmyr@bestlas.se>
2022-09-01 14:21:26 +0000

    Add event listener to circle and test for it

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -55,6 +55,12 @@ const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../g
     test("should display 0 for the element with id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
     });
+    test("expect data listener to be true", () => {
+        const elements = document.getElementsByClassName("circle");
+        for (let element of elements) {
+            expect(element.getAttribute("data-listener")).toEqual("true");
+        }
+    });
  });
 
  describe("gameplay works correctly", () => {
